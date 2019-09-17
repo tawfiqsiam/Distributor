450513e7b26c6644e6a0348b405881f8864e6396
ProGamerYT001 <55412636+ProGamerYT001@users.noreply.github.com>
2019-09-17 20:47:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -58,43 +58,6 @@ client.on('message', msg => {
         msg.reply(serverembed);
     }
 });
-client.on("message", async message => {
-    const prefix = "_";
-
-    if (message.author.bot) return;
-    if (!message.guild) return;
-    if (!message.content.startsWith(prefix)) return;
-
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const cmd = args.shift().toLowerCase();
-
-    if (cmd === "ping") {
-        const msg = await message.channel.send(`🏓 Pinging....`);
-
-        msg.edit(`🏓 Pong!\nLatency is ${Math.floor(msg.createdTimestap - message.createdTimestap)}ms\nAPI Latency is ${Math.round(client.ping)}ms`);
-    }
-
-    if (cmd === "say") {
-        if (message.deletable) message.delete();
-
-        if (args.length < 0) return message.reply(`Nothing to say?`).then(m => m.delete(5000));
-        
-        const roleColor = message.guild.me.highestRole.hexColor;
-        
-        if (args[0].toLowerCase() === "embed") {
-            const embed = new RichEmbed()
-                .setDescription(args.slice(1).join(" "))
-                .setColor(roleColor === "#000000" ? "#ffffff" :  roleColorv)
-                .setTimestamp()
-                .setImage(client.user.displayAvatarURL)
-                .setAuthor(message.author.username, message.author.displayAvatarURL);
-
-            message.channel.send(embed);
-        } else {
-            message.channel.send(args.join(" "));
-        }
-    }
-});
 client.on('message', message =>{
     if (!message.guild) return;
 if (message.content === '=spotify'){
